bfaf15eaac4ab694c6d545adb238293e1fb3575b
Praveenram Balachandran <praveenrambalu@gmail.com>
2020-06-05 10:48:03 +0530

    about change

--- resources/views/inc/scroll-to-top.blade.php
+++
@@ -1,6 +0,0 @@
-     <!-- Scroll [to top] -->
-     <div id="scroll-to-top" class="scroll-to-top">
-        <a href="#slider" class="smoth-anchor">
-            <i class="icon-arrow-up"></i>
-        </a>
-    </div>
